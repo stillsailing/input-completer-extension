782247251f773c51e3201b5e118bbe6d556a1c8c
chenyh <chenyh.site@gmail.com>
2024-09-15 16:29:51 +0800

    feat: list store

--- options/index.tsx
+++ options/index.tsx
@@ -4,10 +4,14 @@ import "~/css/base.css"
 import { useState } from "react"
 
 import { useListStore } from "~/store/list"
+import uuid from "~/util/uuid"
 
 function OptionsIndex() {
-  const [data, setData] = useState("")
   const store = useListStore()
+
+  const [addMode, setAddMode] = useState(false)
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
 
   return (
     <div className="mx-auto max-w-5xl pt-6 px-4">
@@ -15,9 +19,10 @@ function OptionsIndex() {
         Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
       </h1>
       <h2>This is the Option UI page!</h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <div>
-        <button className="btn btn-primary btn-sm" onClick={() => {}}>
+      <div className="mt-4">
+        <button
+          className="btn btn-primary btn-sm"
+          onClick={() => setAddMode(true)}>
           新增
         </button>
         <button className="btn btn-sm ml-2" onClick={() => {}}>
@@ -41,20 +46,57 @@ function OptionsIndex() {
           <tbody>
             {store.list.map((item) => (
               <tr key={item.id} className="hover">
-                <th>
+                <td>
                   <label>
                     <input type="checkbox" className="checkbox" />
                   </label>
-                </th>
+                </td>
                 <td>{item.title}</td>
                 <td>{item.completeText}</td>
                 <td>
-                  <button className="btn btn-link p-0 my-0" onClick={() => {}}>
+                  <button
+                    className="btn btn-link p-0 my-0"
+                    onClick={() => store.remove(item.id)}>
                     删除
                   </button>
                 </td>
               </tr>
             ))}
+            {addMode && (
+              <tr>
+                <td></td>
+                <td>
+                  <input
+                    className="input input-bordered input-sm w-full max-w-xs"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </td>
+                <td>
+                  <input
+                    className="input input-bordered input-sm w-full max-w-xs"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                  />
+                </td>
+                <td>
+                  <button
+                    className="btn btn-link p-0 my-0"
+                    onClick={() => {
+                      store.add({
+                        id: uuid(),
+                        title,
+                        completeText: content
+                      })
+                      setAddMode(false)
+                      setTitle("")
+                      setContent("")
+                    }}>
+                    确定
+                  </button>
+                </td>
+              </tr>
+            )}
           </tbody>
         </table>
       </main>
--- store/list.ts
+++ store/list.ts
@@ -1,54 +1,58 @@
 import { create } from "zustand"
-import { persist } from "zustand/middleware"
+import { persist, type PersistStorage } from "zustand/middleware"
 
-const chromeStorage = {
-  getItem: (name) =>
-    new Promise((resolve) => {
-      chrome.storage.local.get([name], (result) => {
-        resolve(result[name])
-      })
-    }),
-  setItem: (name, value) =>
-    new Promise<void>((resolve) => {
-      chrome.storage.local.set({ [name]: value }, () => {
-        resolve()
-      })
-    }),
-  removeItem: (name) =>
-    new Promise<void>((resolve) => {
-      chrome.storage.local.remove([name], () => {
-        resolve()
-      })
-    })
+import { Storage } from "@plasmohq/storage"
+
+export const storage = new Storage({ area: "sync" })
+
+const storageAdaptor = {
+  getItem: async (name: string) => {
+    const result = await storage.get<string>(name)
+    return result ?? null
+  },
+  setItem: async (name: string, value: string) => {
+    await storage.set(name, value)
+  },
+  removeItem: async (name: string) => {
+    await storage.remove(name)
+  }
 }
 
 interface Item {
-  id: number
+  id: string
   title: string
   completeText: string
 }
 
+interface Store {
+  list: Item[]
+  add: (item: Item, index?: number) => void
+  remove: (id: string) => void
+}
+
 export const useListStore = create(
-  persist(
-    (set) => ({
-      list: [
-        {
-          id: 1,
-          title: "test-1",
-          completeText: "test-1-complete"
-        },
-        {
-          id: 2,
-          title: "test-2",
-          completeText: "test-2-complete"
+  persist<Store>(
+    (set, get) => ({
+      list: [],
+      add: (item: Item, index?: number) => {
+        if (index === undefined) {
+          set((state) => ({ list: [...state.list, item] }))
+          return
         }
-      ] as Item[],
-      add: (index: number, item: Item) =>
-        set((list) => [...list.slice(0, index), item, ...list.slice(index)])
+        set((state) => ({
+          list: [
+            ...state.list.slice(0, index),
+            item,
+            ...state.list.slice(index)
+          ]
+        }))
+      },
+      remove: (id: string) =>
+        set((state) => ({ list: state.list.filter((i) => i.id !== id) }))
     }),
     {
       name: "zustand-chrome-storage",
-      storage: chromeStorage
+      storage: storageAdaptor as unknown as PersistStorage<Store>
     }
   )
 )
